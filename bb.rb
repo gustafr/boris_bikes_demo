1a1280afb899a813c46de199d331ae2aa2ef6cfd
tochman <thomas@nocebo.se>
2015-09-04 14:06:10 +0200

    added bb module to load all classes

---
+++ bb.rb
@@ -0,0 +1,8 @@
+module BorisBikes 
+  require './lib/station.rb'
+  require './lib/bike.rb'
+  require './lib/bike_container.rb'
+  require './lib/garage.rb'
+  require './lib/person.rb'
+  require './lib/van.rb'
+end
